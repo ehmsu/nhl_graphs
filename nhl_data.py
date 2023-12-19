c9931ac1298431d9a97a200a9af16eb63642fb3e
ehmsu <amelydotsu@gmail.com>
2023-12-19 09:35:59 -0500

    filename shenanigans

--- nhl_data.py
+++ nhl_data.py
@@ -1,15 +1,16 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import numpy as np 
+import matplotlib.ticker as mtick
+import os 
 
-fig, ax = plt.subplots(1, 1)
+fig1, ax1 = plt.subplots(1, 1)
 
-# moneypuck data 
-# moneypuck_data = pd.read_csv(r"/Users/user/Documents/Programs/nhl/all_teams.csv", header = 0)
-# seasons_moneypuck = moneypuck_data.groupby(moneypuck_data["season"]) # group by seasons
+filepath = os.path.abspath(os.getcwd()) + "/logos/"
+print(filepath)
 
 # nhl data 
-nhl_edge_data = pd.read_csv(r"/Users/user/Documents/Programs/nhl/NHL-Edge-Data-Teams-21-23.csv", header = [0, 1])
+nhl_edge_data = pd.read_csv(os.path.abspath(os.getcwd())+r"/NHL-Edge-Data-Teams-21-23.csv", header = [0, 1])
 seasons_nhl_edge = nhl_edge_data.groupby(nhl_edge_data["Season", "Season"]) # group by seasons
 
 # convert Zone Time OZ% to OZ as a float 
@@ -24,7 +25,6 @@ print(nhl_edge_data["Zone Time", "OZ%"])
 # nhl_data.replace(to_replace=nhl_data["Zone Time", "OZ%"], value=new_oz)
 
 # 21-22 Season (Regular)
-    
 reg_season_21_22 = seasons_nhl_edge.get_group("21-22")
 
 # 21-22 Season (Playoffs)
@@ -39,14 +39,15 @@ playoff_season_22_23 = seasons_nhl_edge.get_group("22-23p")
 # Zone time comparison 
 
 # print(reg_season_21_22["Zone Time", "OZ%"])
-ax.set_title("Time spent in OZ of PP")
-ax.scatter(reg_season_21_22["Abbreviation", "Abbreviation"], reg_season_21_22["Zone Time", "PPOZ"], label="Regular Season 2021-2022")
-ax.scatter(reg_season_22_23["Abbreviation", "Abbreviation"], reg_season_22_23["Zone Time", "PPOZ"], label="Regular Season 2022-2023")
-# ax.scatter(nhl_data["Abbreviation", "Abbreviation"], nhl_data["Skating Speed", "22+"])
-# ax.scatter(nhl_data["Abbreviation"], nhl_data["PP"])
-ax.set_xlabel("Teams")
-ax.set_ylabel("% PPOZ Time")
-ax.legend()
+fmt = '%.1f%%'
+yticks = mtick.FormatStrFormatter(fmt)
+ax1.yaxis.set_major_formatter(yticks)
+ax1.set_title("Time spent in OZ of PP")
+ax1.scatter(reg_season_21_22["Abbreviation", "Abbreviation"], reg_season_21_22["Zone Time", "PPOZ"], label="Regular Season 2021-2022")
+ax1.scatter(reg_season_22_23["Abbreviation", "Abbreviation"], reg_season_22_23["Zone Time", "PPOZ"], label="Regular Season 2022-2023")
+ax1.set_xlabel("Teams")
+ax1.set_ylabel("% PPOZ Time")
+ax1.legend()
 
 plt.show()
 
