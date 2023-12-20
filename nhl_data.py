b6be566779d8d1f2bc9c45f53c7901a4b0da3a18
ehmsu <amelydotsu@gmail.com>
2023-12-20 14:03:24 -0500

    adding an x axis

--- nhl_data.py
+++ nhl_data.py
@@ -23,7 +23,10 @@ def plot_images(x, y, ax=None):
     ax = ax or plt.gca()
 
     for xi, yi in zip(x,y):
-        path = get_logos(xi)
+        # print(xi)
+        index = np.where([x == xi])[1][0]
+        # print(index)
+        path = get_logos(nhl_edge_data['Abbreviation', 'Abbreviation'].values[index])
         image = Image.open(path)
         image.thumbnail((60, 60), Image.Resampling.LANCZOS)
         im = OffsetImage(image, zoom=72/ax.figure.dpi)
@@ -35,20 +38,20 @@ def plot_images(x, y, ax=None):
 
 # percentages = ["PPOZ", "OZ%"]
 
-def graph_nhl(season:str, category:str, subcategory:str):
+def graph_nhl(season:str, category1:str, subcategory1:str, category2:str, subcategory2:str):
     fig1, ax1 = plt.subplots(1, 1)
-    if '%' in subcategory:
+    if '%' in subcategory2:
         fmt = '%.1f%%'
         yticks = mtick.FormatStrFormatter(fmt)
         ax1.yaxis.set_major_formatter(yticks)
-    ax1.set_title(season + " " + category + ": " + subcategory)
-    ax1.set_xticks([]) # remove x ticks
-    ax1.set_ylabel(category+": "+subcategory)
+    ax1.set_title(season + " " + category1 + ": " + subcategory1 + " vs. " + category2 + ": " + subcategory2)
+    ax1.set_ylabel(category2+": "+subcategory2)
+    ax1.set_xlabel(category1+": "+subcategory1)
     season_group = seasons_nhl_edge.get_group(season)
-    ax1.plot(season_group["Abbreviation", "Abbreviation"], season_group[category, subcategory], linestyle="None")
-    plot_images(season_group["Abbreviation", "Abbreviation"], season_group[category, subcategory], ax=ax1)
+    ax1.plot(season_group[category1, subcategory1], season_group[category2, subcategory2], linestyle="None")
+    plot_images(season_group[category1, subcategory1], season_group[category2, subcategory2], ax=ax1)
 
-graph_nhl("21-22", "Shot Speed", "90+ per 60")
+graph_nhl("21-22", "Zone Time", "PPOZ%", "Shot Speed", "90+ per 60")
 
 plt.show()
 
